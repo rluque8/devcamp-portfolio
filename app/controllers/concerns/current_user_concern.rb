e8724f47a83df83ee8d5585925def2674638db32
Rodrigo Luque <RodrigoLuque@MacBook-Pro-de-Rodrigo-3.local>
2018-07-27 11:53:32 +0200

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,4 +1,4 @@
-module CurrentUserConcern
+ module CurrentUserConcern
   extend ActiveSupport::Concern
 
   def current_user
@@ -8,9 +8,16 @@ module CurrentUserConcern
   end
 
   def guest_user
-    OpenStruct.new(name: "Guest User", 
-                  first_name: "Guest",
-                  last_name:"User", 
-                  email:"guest@example.com")
+    guest = GuestUser.new
+    guest.name ="Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest #In order to return the entire Object guest
+    
+    #OpenStruct.new(name: "Guest User", 
+    #              first_name: "Guest",
+    #             last_name:"User", 
+    #             email:"guest@example.com")
   end
 end
